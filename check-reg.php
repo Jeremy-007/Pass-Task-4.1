c7eb94816abdb10a191c4a9c0bd46dc6a527d4ae
Jeremy-007 <101228829@students.swinburne.edu.my>
2021-04-29 19:05:41 +0800

    correction for php files

--- check-reg.php
+++ check-reg.php
@@ -1,13 +1,13 @@
     <?php
 
-    // echo $_POST['input_nric'];        
+    // echo $_POST['input_nric'];
 
-    if(file_exists($_POST ['icno'] . '.txt')){
+    if (file_exists($_POST ['icno'] . '.txt')) {
             echo '<script type="text/javascript">';
             echo ' alert("You are already registered!");';
             echo 'window.location.href = "page4.html";';
             echo '</script>';
-    }
+    } 
     else {
         echo '<script type="text/javascript">';
         echo ' alert("You have not been registered yet!");';
@@ -16,4 +16,4 @@
     }
 
     ?>
-	
+ 
--- formextract.php
+++ formextract.php
@@ -1,16 +1,12 @@
-<?php
+    <?php
 
-    
-       if (file_exists($_POST['icno'] . '.txt')){     //Check for filename with given nric
-            echo '<script type="text/javascript">';
-            echo ' alert("This IC Number have been registered!");';
-            echo 'window.location.href = "page4.html";';
-            echo '</script>';
-           
-           
-        }
-        else{   //filename not found, write to file
-                
+if (file_exists($_POST['icno'] . '.txt')) {      //Check for filename with given nric
+    echo '<script type="text/javascript">';
+    echo ' alert("This IC Number have been registered!");';
+    echo 'window.location.href = "page4.html";';
+    echo '</script>';
+} 
+else {   //filename not found, write to file     
            $content_to_write = "First Name: " . $_POST['fname'] . "\n";
            $content_to_write .= "Last Name: " . $_POST['lname'] . "\n";
            $content_to_write .= "IC Number/Passport ID : " . $_POST['icno'] . "\n";
@@ -27,9 +23,5 @@
            file_put_contents($_POST['icno'] . '.txt', $content_to_write);
             
         header('Location: thank_you_page.html');
-       
-        }
-
-    
-
-?>
+        } 
+    ?>
