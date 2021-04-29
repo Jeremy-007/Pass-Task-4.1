7bd1ca3c5f958c25ed47021bd317a98a802569bb
AaronRJ99 <101229165@students.swinburne.edu.my>
2021-04-29 17:14:37 +0800

    added data checked php

---
+++ check-reg.php
@@ -0,0 +1,18 @@
+    <?php
+        
+        // echo $_POST['input_nric'];
+        
+        if (file_exists($_POST['icno'] . '.txt')){
+            echo '<script type="text/javascript">';
+            echo ' alert("You are already registered!");';
+            echo 'window.location.href = "page4.html";';
+            echo '</script>';
+        }
+        else{
+             echo '<script type="text/javascript">';
+            echo ' alert("You have not been registered yet!");';
+            echo 'window.location.href = "page4.html";';
+            echo '</script>';
+        }
+
+    ?>
